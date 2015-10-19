995b07c24690d1e369cf2364efcd2d30c2969679
Samuel Lie <samuellie@Samuels-MacBook-Pro.local>
2015-10-19 19:00:06 +0200

    Front-end fixes

--- _System/assets/js/components/active-nav.js
+++ _System/assets/js/components/active-nav.js
@@ -5,3 +5,18 @@ $(function() {
       scrollSpeed: 2500
   });
 });
+
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if ($(window).width() > 640) {
+    if (y > $(window).height() -54) {
+      $('.symbol').show();
+    } else {
+      $('.symbol').hide();
+    }
+  }
+});
+
+$(document).scroll(function() {
+  console.log($(this).scrollTop());
+});
--- _System/assets/js/components/navigation.js
+++ _System/assets/js/components/navigation.js
@@ -1,9 +1,4 @@
 $( "#navToggle" ).click(function() {
   $( "#mobileNav" ).toggleClass( "active" );
-  $( this ).toggleClass( "active" );
-});
-
-$( "#mobileNav a" ).click(function() {
-  $( "#mobileNav" ).toggleClass( "active" );
-  $( "#navToggle" ).toggleClass( "active" );
+  $( this ).toggleClass( "active, close" );
 });
