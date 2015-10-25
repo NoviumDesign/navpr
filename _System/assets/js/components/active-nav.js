329635e44ad33b18554bffccfa63b4f8d5a03d66
Anton Kjörck Lindén <anton@noviumdesign.se>
2015-10-25 11:42:12 +0100

    Reordering some text + I hate IE....

--- _System/assets/js/components/active-nav.js
+++ _System/assets/js/components/active-nav.js
@@ -1,22 +1,16 @@
 $(function() {
-  $('#desktopNav').onePageNav({
-      currentClass: 'active',
-      scrollThreshold: 0.28,
-      scrollSpeed: 2500
-  });
-});
-
-$(document).scroll(function() {
-  var y = $(this).scrollTop();
-  if ($(window).width() > 640) {
-    if (y > $(window).height() -54) {
-      $('.symbol').show();
-    } else {
-      $('.symbol').hide();
+  $(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if ($(window).width() > 640) {
+      if (y > $(window).height() -54) {
+        $('.symbol').show();
+      } else {
+        $('.symbol').hide();
+      }
     }
-  }
-});
+  });
 
-$(document).scroll(function() {
-  console.log($(this).scrollTop());
+  $(document).scroll(function() {
+    console.log($(this).scrollTop());
+  });
 });
--- _System/assets/js/components/helpers.js
+++ _System/assets/js/components/helpers.js
@@ -4,6 +4,7 @@ $(function() {
 
     if ($(window).width() > 640) {
       $('.min-height-100').css('min-height', $(window).height() -54);
+      $('.height-100').css('height', $(window).height() -54);
       $('.min-height-100-desktop').css('min-height', $(window).height() -54);
     }
   }
